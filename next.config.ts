1b5dd9f315dc38a53cbe58ddba86875d1567b4e0
jessepunkspin <jessepunkspin@gmail.com>
2025-11-27 07:12:08 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: true,
+    optimizePackageImports: ["@coinbase/onchainkit"],
+  },
+
+  images: {
+    unoptimized: true, // required for Farcaster MiniApps
+  },
+
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
